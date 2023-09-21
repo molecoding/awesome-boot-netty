cb5ca511d02b8eb377c6edf62cbeefe176d31bb3
Jerry Shang <jerryshang@gmail.com>
2023-09-21 15:08:27 +0800

    chore: commit save point

--- src/test/java/com/molecoding/nobs/server/CommandResourceTest.java
+++ src/test/java/com/molecoding/nobs/server/CommandResourceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.OutputStream;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class CommandResourceTest {
   @Autowired
   MockMvc mvc;
